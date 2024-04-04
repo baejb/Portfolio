129fb3ca1b347e9d06fb97404ea7997cb61cb167
baejb <bjb6478@naver.com>
2024-04-04 22:43:42 +0900

    feat: 기초셋팅

---
+++ Portfolio_jb/src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+import LandingPage from './pages/LandingPage'
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* <div>
+        <img src="./Ventura_dark1.png"></img>
+      </div> */}
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
