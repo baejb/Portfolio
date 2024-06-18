733f4c5dbfdbfe009a3f0a9b416d6919082c1265
baejb <bjb6478@naver.com>
2024-06-18 20:41:10 +0900

    style: 폰트 설정

--- Portfolio_jb/src/pages/MainPage.jsx
+++ Portfolio_jb/src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import background from '../img/Ventura_dark1.png'
 import NavBar from '../components/navbar/NavBar';
+import '../font.css';
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: url(${background}); 
     background-size: cover;
+    font-family: "KCC-Hanbit";
    
 `;
 const MainPage = () => {
