851bdc9838a4dd3df700807a29e0121f9ce6c2d2
baejb <bjb6478@naver.com>
2024-06-18 20:39:23 +0900

    feat : common 프로필 이미지 생성

---
+++ Portfolio_jb/src/components/common/ProfileImg.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+import Profile from "../../img/profile.png"
+
+const ProfileImg = ({ width = "150px", height = "150px" }) => {
+    return (
+      <Container style={{ width, height }}>
+        <Image src={Profile} />
+      </Container>
+    );
+  };
+
+export default ProfileImg;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: top;
+  box-shadow: 1px 1px 8px #80808097;
+`;
