55306d30149e5c8c04777df94afb37a49de1df11
baejb <bjb6478@naver.com>
2024-06-19 01:56:15 +0900

    style : 그림자 스타일 변경

--- Portfolio_jb/src/components/navbar/Profile/ProfileIntro.jsx
+++ Portfolio_jb/src/components/navbar/Profile/ProfileIntro.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Intro = styled.div`
     font-size:0.9rem;
-    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.3);
+    text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.3);
 
 `
 const Text = '"3년 동안 웹 프론트엔드 개발을 하고있는 개발자 배정빈입니다."'
